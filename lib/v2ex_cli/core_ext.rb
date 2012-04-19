23b765da7ee7096d284a2dbaacb25c08943e449c
Forrest Ye <afu@forresty.com>
2012-04-19 12:42:35 +0800

    better recursively_symbolize_keys

--- lib/v2ex_cli/core_ext.rb
+++ lib/v2ex_cli/core_ext.rb
@@ -1,17 +1,28 @@
+# adapted from http://devblog.avdi.org/2009/07/14/recursively-symbolize-keys/
+module RecursiveSymbolization
+  def symbolize_value(value)
+    case value
+    when Hash then value.recursively_symbolize_keys
+    when Array then value.recursively_symbolize_keys
+    else value
+    end
+  end
+end
+
 class Hash
-  # adapted from http://devblog.avdi.org/2009/07/14/recursively-symbolize-keys/
+  include RecursiveSymbolization
   def recursively_symbolize_keys
     inject({}) do |result, (key, value)|
-      new_key = case key
-                when String then key.to_sym
-                else key
-                end
-      new_value = case value
-                  when Hash then value.recursively_symbolize_keys
-                  else value
-                  end
-      result[new_key] = new_value
+      new_key = (key.is_a? String) ? key.to_sym : key
+      result[new_key] = symbolize_value(value)
       result
     end
+  end
+end
+
+class Array
+  include RecursiveSymbolization
+  def recursively_symbolize_keys
+    map { |item| symbolize_value(item) }
   end
 end
--- spec/v2ex_cli/core_ext_spec.rb
+++ spec/v2ex_cli/core_ext_spec.rb
@@ -3,14 +3,35 @@ require "spec_helper"
 describe Hash do
   subject { Hash.new }
   it { should respond_to :recursively_symbolize_keys }
+
   context "recursively_symbolize_keys" do
     it "should symbolize keys" do
       expected = { :key => 'value' }
       { 'key' => 'value' }.recursively_symbolize_keys.should == expected
     end
+
     it "should do it recursively" do
       expected = { :drinks => { :martini => { :garnish => "olive" }, :gibson => { :garnish => "onion" }}}
       hash = { "drinks" => { "gibson" => { "garnish" => "onion" }, "martini" => { "garnish" => "olive" }}}
+
+      hash.recursively_symbolize_keys.should == expected
+    end
+  end
+end
+
+describe Array do
+  subject { Array.new }
+  it { should respond_to :recursively_symbolize_keys }
+
+  context "recursively_symbolize_keys" do
+    it "should symbolize keys" do
+      expected = [{ :key1 => 'value1' }, { :key2 => 'value2' }]
+      [{ 'key1' => 'value1' }, { 'key2' => 'value2' }].recursively_symbolize_keys.should == expected
+    end
+
+    it "should do it recursively" do
+      expected = { :drinks => [{ :martini => "olive" }, { :gibson => "onion" }] }
+      hash = { "drinks" => [{ "martini" => "olive" }, { "gibson" => "onion" }] }
 
       hash.recursively_symbolize_keys.should == expected
     end
